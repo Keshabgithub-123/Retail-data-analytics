38dd502a180ff5ded0e5f59cbe44bcd9d2ed4bc9
Keshabgithub-123 <73981343+Keshabgithub-123@users.noreply.github.com>
2026-02-21 20:20:26 +0530

    Updated orders data analysis.py

--- orders data analysis.py
+++ orders data analysis.py
@@ -1,57 +1,31 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-#import Libraries
-!python -m pip install pandas sqlalchemy pyodbc
-
- 
-#Read data from the file and handle null values
 import pandas as pd
-df = pd.read_csv('orders.csv',na_values=['Not Available', 'unknown'])
-df['Ship Mode'].unique()
 
+# Load data
+df = pd.read_csv('orders.csv', na_values=['Not Available', 'unknown'])
 
-array(['Second Class', 'Standard Class', nan, 'First Class', 'Same Day'],
-      dtype=object)
+# Clean column names
+df.columns = df.columns.str.lower()
+df.columns = df.columns.str.replace(' ', '_')
 
+# Create new columns
+df['discount'] = df['list_price'] * df['discount_percent'] * 0.01
+df['sale_price'] = df['list_price'] - df['discount']
+df['profit'] = df['sale_price'] - df['cost_price']
 
-#Rename columns names and make them lower case and replace space with underscore
-#df.rename(columns={'Order Id':'order_id', 'City': 'city'}) 
-df.columns=df.columns.str.lower()
-df.columns=df.columns.str.replace(' ','_')
-df.head(5)
+# Convert date
+df['order_date'] = pd.to_datetime(df['order_date'])
 
-#Drive new columns discount, sale price and profit
-df['discount']=df['list_price']*df['discount_percent']*.01
-df['sale_price']=df['list_price']-df['discount']
-df['profit']=df['sale_price']-df['cost_price']
-df.head(5)
-
-
-#convert order date from object data type to datetime
-#df.dtypes
-df['order_date']=pd.to_datetime(df['order_date'],format="%Y-%m-%d")
-
-
-#drop cost price, list price and discount percent columns
-#df.drop(['list_price', 'cost_price', 'discount_percent'], axis=1, inplace=True)
-df.columns
-
-
+# Connect to SQL
 from sqlalchemy import create_engine
+
 engine = create_engine(
     "mssql+pyodbc://@localhost/mydatabase?driver=ODBC+Driver+18+for+SQL+Server&trusted_connection=yes&TrustServerCertificate=yes"
 )
 
 conn = engine.connect()
 print("Connected successfully")
-Connected successfully
-import pyodbc
-print(pyodbc.drivers())
 
+# Load to SQL
+df.to_sql('orders_data', con=engine, index=False, if_exists='replace', schema='dbo')
 
-df.to_sql('orders_data', con=engine, index=False, if_exists='replace')
-
- 
- 
+print("Data uploaded successfully")
